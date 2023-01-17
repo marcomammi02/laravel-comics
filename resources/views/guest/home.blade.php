d7408e0f304e24163e6d06db824995bcd75219af
marcomammi02 <marcomammi2002@gmail.com>
2023-01-17 14:30:51 +0100

    first commit

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,5 @@
+@extends('guest.layouts.base')
+
+@section('content')
+
+@endsection
---
+++ resources/views/guest/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="additional-dc-sites-section">
+        <p>ADDITIONAL DC SITES &#9660;</p>
+        <p>DC POWER™ VISA®</p>
+    </div>
+    <div class="link-logo-section">
+        <img src="{{ asset('img/dc-logo.png') }}" alt="Logo">
+        <ul class="header-menu">
+          <li>CHARACTER</li>
+          <li>COMICS</li>
+          <li>MOVIES</li>
+          <li>TV</li>
+          <li>GAMES</li>
+          <li>COLLECTIBLES</li>
+          <li>VIDEOS</li>
+          <li>FANS</li>
+          <li>NEWS</li>
+          <li>SHOP</li>
+        </ul>
+    </div>
+</header>
